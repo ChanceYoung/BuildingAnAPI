aa6b08c89d68777f5edbe05e010335671bdf3223
kyler daybell <kyler.daybell96@gmail.com>
2020-03-21 08:46:57 -0600

    running express

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log("hello world");
+var express = require('express');
+var app = express();
+
+
+app.get('/', function(req, res){
+    let hello = {string:"Hello World"}
+    res.write(JSON.stringify(hello))
+    res.end()
+})
+
+
+var server = app.listen(80, function(){
+    console.log("Running on " + 80)
+})
